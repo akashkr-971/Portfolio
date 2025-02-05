7c066c418d300bd7e6090cefcd09391633c424ab
Akash kr <akashkr971@gmail.com>
2025-02-05 17:04:08 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -25,7 +25,7 @@ function Contact() {
         setStatus('');
 
         try {
-            const response = await fetch('http://localhost:5000/api/contact', {
+            const response = await fetch('https://akashkrportfolio.vercel.app/api/contact', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
